a6fccf3c0e3ed96bd2400a84560f42ef4636b70b
paulana <marie@pthompson.co>
2017-06-12 12:36:19 -0400

    Create algorithms solution and console app project

---
+++ algorithms/algorithms/Algorithms.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithms
+{
+    class ShadiddleMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Shadiddles!  Pick your fiddles:");
+            Console.WriteLine("Type 'intro' for an overview or 'help' for list of options.");
+            string input = Console.ReadLine();
+            string[] quit =
+            {
+                "q",
+                "quit",
+                "exit",
+                "x"
+            };
+
+            while (String.IsNullOrWhiteSpace(input) == false && quit.Contains(input.Trim().Split(' ')[0]) == false)
+            {
+                switch (input.Trim().Split(' ')[0])
+                {
+                    case "intro":
+                        Shadiddle.Demo();
+                        break;
+
+                    case "h" :
+                    case "help" :
+                    case "?" :
+                    case "man" :
+                        Shadiddle.Help();
+                        break;
+
+                    case "list":
+                        Shadiddle.Questions(input.Trim().Split(' ')[1]);
+                        break;
+
+                    default:
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+
+        
+    }
+}
